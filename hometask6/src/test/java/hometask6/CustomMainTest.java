f686686ea384060ab5f79527854bb0e6f52f1a16
chonithya <51079543+chonithya@users.noreply.github.com>
2020-03-05 21:41:58 +0530

    Add files via upload

---
+++ hometask6/src/test/java/hometask6/CustomMainTest.java
@@ -0,0 +1,14 @@
+package hometask6;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CustomMainTest
+{
+      @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
